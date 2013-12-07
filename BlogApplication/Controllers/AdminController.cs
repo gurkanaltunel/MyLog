ee393e921c5ab3860be60e97c0fa6f2482894a04
gurkanaltunel <grknaltunel@gmail.com>
2013-12-07 15:11:05 +0200

    adding admin control

---
+++ BlogApplication/Controllers/AdminController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogApplication.Models;
+
+namespace BlogApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+ 
+        BlogDbContext db = new BlogDbContext();
+
+        public ActionResult Index()
+        {
+            List<Article> list = new List<Article>();
+            var articles = db.Articles.Select(x => new { Id = x.Id, Title = x.Title });
+            foreach (var item in articles)
+            {
+                list.Add(
+        new Article
+        {
+            Id = item.Id,
+            Title = item.Title
+        }
+        );
+            }
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            try
+            {
+                db.Articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Admin/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Admin/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
